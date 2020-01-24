3dc18bba19b80327dc6f01a74fb200acf38103a6
Quentin Crida <qcrida@gmail.com>
2020-01-24 16:19:08 +0000

    operator buttons tested

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -10,10 +10,22 @@ describe('calculator functionality', function() {
   });
 
   // write integration tests here in the form of "it should do something..."
-  it('should have working number buttons', function(){
+  it('it should have working number buttons', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
-  })
+  }),
+ // 2. Do the arithmetical operations update the display with the result of the operation?
+ it('it should have working operator buttons', function(){
+   running_total = element(by.css('#running_total'))
+   element(by.css('#number2')).click();
+   element(by.css('#operator_add')).click();
+   element(by.css('#number2')).click();
+   element(by.css('#operator_equals')).click();
+   expect(running_total.getAttribute('value')).to.eventually.equal('4')
+ })
+ // 3. Can multiple operations be chained together?
+ // 4. Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
+ // 5. What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
 
 });
